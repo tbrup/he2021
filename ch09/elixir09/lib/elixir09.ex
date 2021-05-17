dad3c28d9ed99cd7c8b8720a408b09eddb27c6ab
Thomas Brupbacher <thomas.brupbacher@protonmail.ch>
2021-05-17 21:31:36 +0200

    Ch09: multi-core with elixir09.ex
    The fastest version so far: multi-core using flow, 33s (only 60% of the
    rust wall time).

--- ch09/elixir09/lib/elixir09.ex
+++ ch09/elixir09/lib/elixir09.ex
@@ -2,6 +2,8 @@ defmodule Elixir09 do
   @moduledoc """
   Documentation for `Elixir09`.
   """
+  import ExProf.Macro
+  use Flow
 
   @doc """
   Hello world.
@@ -16,10 +18,36 @@ defmodule Elixir09 do
     IO.puts :world
   end
 
+  def run(100000000) do
+    IO.puts "done"
+  end
+
+  def run(i) do
+    Ch09.search(i)
+    run(i+1)
+  end
+
+  def notempty({:ok, s}) do
+    String.length(s) > 0
+  end
+
   def main do
-    # Enum.each(10000000..100000000, &Flow.from_enumerable/1)
-    #|> Flow.map(&Ch09.search/1)
-    Enum.each(10000000..100000000, &Ch09.search/1)
-  #   |> Flow.from_enumerable()
+    # 
+    # working version with Stream, 249s
+    # 10000000..100000000 |>
+    # Stream.map(&Ch09.searchstr/1) |>
+    # Stream.filter(&notempty/1) |>
+    # Enum.to_list |>
+    # IO.inspect
+    #
+    # Flow version: 33s (6 cores, 12 threads)
+    profile do
+      10000000..100000000 |>
+      Flow.from_enumerable() |>
+      Flow.map(&Ch09.searchstr/1) |>
+      Flow.filter(&notempty/1) |>
+      Enum.to_list |>
+      IO.inspect
+    end
   end
 end
--- ch09/elixir09/lib/search.ex
+++ ch09/elixir09/lib/search.ex
@@ -1,4 +1,19 @@
 defmodule Ch09 do
+  use Bitwise
+
+  def _nibbles(<<>>, <<tmp::binary()>>) do
+    tmp
+  end
+
+  def _nibbles(<<c::8,rest::binary()>>, tmp) do
+    h1 = (c &&& 0xf0) >>> 4
+    l1 = (c &&& 0x0f)
+    _nibbles(rest, tmp <> <<h1::8>> <> <<l1::8>> )
+  end
+
+  def nibbles(s) do
+    _nibbles(s, <<>>)
+  end
 
   def search (100000000) do
     IO.puts "done...."
@@ -9,13 +24,34 @@ defmodule Ch09 do
       IO.puts "#{i}"
     end
     s = "#{i} Cola Decaf"
-    h = :crypto.hash(:sha256, s) |>Base.encode16
-    #h = :erlsha2.sha256(s)
-    if Regex.match?(~r{C01A}, h) do
-      if Regex.match?(~r{DECAF},h) do
+    h = :crypto.hash(:sha256, s) |> nibbles # |>Base.encode16
+    if String.contains?(h, <<0xC::8, 0::8, 1::8, 0xA::8>>) do
+      if String.contains?(h, <<0xd::8, 0xe::8, 0xC::8, 0xA::8, 0xf::8>>) do
         IO.puts "found match for s: #{s}"
-        IO.puts h
+        :crypto.hash(:sha256, s)|> Base.encode16 |> IO.puts
       end
+    end
+    # if Regex.match?(~r{C01A}, h) do
+    #   if Regex.match?(~r{DECAF},h) do
+    #     IO.puts "found match for s: #{s}"
+    #     IO.puts h
+    #   end
+    # end
+  end
+
+  def searchstr (i) do
+    if rem(i,1000000) == 0 do
+      IO.puts "#{i}"
+    end
+    s = "#{i} Cola Decaf"
+    h = :crypto.hash(:sha256, s) |> nibbles # |>Base.encode16
+    if String.contains?(h, <<0xC::8, 0::8, 1::8, 0xA::8>>) and
+      String.contains?(h, <<0xd::8, 0xe::8, 0xC::8, 0xA::8, 0xf::8>>) do
+        IO.puts "found match for s: #{s}"
+        tmp = :crypto.hash(:sha256, s)|> Base.encode16 
+        {:ok, "found match for s; #{s}\n#{tmp}"}
+    else
+    {:ok, ""}
     end
   end
 end
